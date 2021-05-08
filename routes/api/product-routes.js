be37d2cdde4cdd2ce04aa82e9b7b4a6ff70f4083
Corbin Garner <Corbingar@gmail.com>
2021-05-08 13:02:55 -0700

    Edited Routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+    try {
+
+
+        res.status(200).json(productData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+    try {
+
+
+        res.status(200).json(productData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // create new product
@@ -90,7 +100,14 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+    try {
+
+
+
+    res.status(200).json(results);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
