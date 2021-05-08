0659086e1ad11e9b0a09769fa790a12228c17a71
Corbin Garner <Corbingar@gmail.com>
2021-05-08 13:43:48 -0700

    Edited Routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -55,26 +55,25 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    try {
+        const productData = await Product.create(req.body);
+        if (req.body.tagIds.length) {
+            const productTagIdArr = req.body.tagIds.map((tag_id) => {
+                return {
+                    product_id: productData.id,
+                    tag_id,
+                };
+            });
+            const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
+            res.status(200).json(productTagIds);
+        } else {
+            res.status(200).json(productData);
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
 });
 
 // update product
