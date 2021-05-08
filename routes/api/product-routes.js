44a21349e2b4b3b673f7af692bcd3b7923f79f1e
Corbin Garner <Corbingar@gmail.com>
2021-05-08 13:19:03 -0700

    added find one product to routers

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,9 +6,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
     try {
-
-
-        res.status(200).json(productData);
+        const productData = await Product.findAll({
+            include: [{ model: Category },
+            {
+                model: Tag,
+                through: ProductTag,
+                as: "tags"
+            },
+            ]
+        });
+        res.status(200).json(productData)
     } catch (err) {
         res.status(500).json(err);
     }
@@ -17,7 +24,20 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
     try {
+        const productData = await Product.findByPk((req.params.id, {
+            include: [{ model: Category },
+            {
+                model: Tag,
+                through: ProductTag,
+                as: "tags"
+            },
+            ]
+        });
 
+        if (!productData) {
+            res.status(404).json({ message: `No product was found with id ${req.params.id}` });
+            return;
+        }
 
         res.status(200).json(productData);
     } catch (err) {
