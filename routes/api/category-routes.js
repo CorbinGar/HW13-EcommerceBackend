afb1708aef37b17bc67fe6294645fbc9d07e210a
Corbin Garner <Corbingar@gmail.com>
2021-05-09 20:08:30 -0700

    cat routes changed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,25 +31,31 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    try {
-
-
-
-        res.status(200).json(categoryData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-    try {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
-
-
-        res.status(200).json(categoryData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!categoryData) {
+      res.status(404).json({ message: `No category found with id ${req.params.id}` });
+      return;
     }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
